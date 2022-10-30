9bbe74527f929818133ccb2f54d4f34bfdf6d668
alizad-hadi <alizadahadi2050@gmail.com>
2022-10-30 12:05:55 -0700

    readme file added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Easy Chat
+
+A full-stack Django and React chat application that enables you chat with other participants easily.
+
+## Features
+
+In this project, I have used several packages and implemented functionlities like token authentication, 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,12 @@ import image from '../image/chat.png'
 const Home = () => {
   return (
     <div className="max-w-5xl mx-auto mt-10">
-      <div className="flex items-center justify-between">
-        <h1 className="text-4xl ">Home</h1>
+      <div className="flex justify-between">
+        <div>
+        <h1 className="text-5xl mt-10 font-Inter">Welcome to Easy Chat</h1>
+        <p className="mt-5 text-xl font-Inter">We simplify the way you <span className="text-amber-300">communicate</span> with your friends<sup className="text-3xl">💑</sup></p>
+        </div>
+
         <img src={image} className="w-96 h-96" alt="" />
       </div>
     </div>
